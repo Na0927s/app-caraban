b86a8ca3356225f582612063b9980114e399cd5d
Na0927s <mj46534379@gmail.com>
2025-11-21 01:41:34 +0900

    feat: Completed core Phase 1 MVP features

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleNameMapper: {
+    '^@models/(.*)$': '<rootDir>/src/models/$1',
+    '^@repositories/(.*)$': '<rootDir>/src/repositories/$1',
+    '^@services/(.*)$': '<rootDir>/src/services/$1',
+    '^@exceptions/(.*)$': '<rootDir>/src/exceptions/$1',
+  },
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageReporters: ['json', 'lcov', 'text', 'clover'],
+  collectCoverageFrom: [
+    'src/services/**/*.ts',
+    '!src/services/**/*.d.ts',
+  ],
+};
